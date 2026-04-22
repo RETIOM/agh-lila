a60f81a68952ce0dfea123471c946530ae10bc50
retiq <idzkowskit@gmail.com>
2026-04-22 22:22:19 +0200

    add parser and scanner code

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from agh-lila!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/tokens.py
@@ -0,0 +1,27 @@
+tokens = (
+    'ID', 'INTEGER_LITERAL', 'FLOAT_LITERAL', 'CHAR_LITERAL', 'STRING_LITERAL',
+    
+    'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'MOD',
+    'INC', 'DEC',
+    
+    'EQ', 'NEQ', 'LT', 'GT', 'LE', 'GE',
+    'AND', 'OR', 'NOT',
+    
+    'BIT_AND', 'BIT_OR', 'BIT_XOR', 'BIT_NOT', 'LSHIFT', 'RSHIFT',
+    
+    'ASSIGN', 'ADD_ASSIGN', 'SUB_ASSIGN', 'MUL_ASSIGN', 'DIV_ASSIGN', 'MOD_ASSIGN',
+    
+    'LPAREN', 'RPAREN',
+    'LBRACE', 'RBRACE',
+    'LBRACKET', 'RBRACKET',
+    'COMMA', 'SEMI',
+    'ARROW', 'DOTDOT',
+    
+    'FN', 'RETURN', 
+    'IF', 'ELSE', 
+    'FOR', 'IN', 'WHILE', 'DO',
+    'TRUE', 'FALSE',
+    
+    'TYPE_INT', 'TYPE_DOUBLE', 'TYPE_FLOAT', 'TYPE_CHAR', 'TYPE_BOOL', 'TYPE_VOID'
+)
+
